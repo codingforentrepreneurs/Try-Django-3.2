4e9f811a7ab03037e829cbb8159551ec90c6a77b
CFE <hello@teamcfe.com>
2021-07-20 13:59:39 -0500

    10 - Your first web page

---
+++ trydjango/views.py
@@ -0,0 +1,15 @@
+"""
+To render html web pages
+"""
+from django.http import HttpResponse
+
+HTML_STRING = """
+<h1>Hello World</h1>
+"""
+
+def home(request):
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+    return HttpResponse(HTML_STRING)
