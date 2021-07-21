ef738907b673ce8fb5cbf00803af4c1694cf77a8
CFE <hello@teamcfe.com>
2021-07-21 12:37:52 -0500

    17 - Django Template Basics

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print("BASE_DIR", BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",          
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- trydjango/views.py
+++ trydjango/views.py
@@ -3,7 +3,7 @@ To render html web pages
 """
 import random
 from django.http import HttpResponse
-
+from django.template.loader import render_to_string
 from articles.models import Article
 
 
@@ -18,12 +18,16 @@ def home_view(request):
     # from the database??
     article_obj = Article.objects.get(id=random_id)
 
+    context = {
+        "object": article_obj,
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content
+    }
     # Django Templates
-    H1_STRING = f"""
-    <h1>{article_obj.title} (id: {article_obj.id})!</h1>
-    """
-    P_STRING = f"""
-    <p>{article_obj.content}!</p>
-    """
-    HTML_STRING = H1_STRING + P_STRING
+    HTML_STRING = render_to_string("home-view.html", context=context)
+    # HTML_STRING = """
+    # <h1>{title} (id: {id})!</h1>
+    # <p>{content}!</p>
+    # """.format(**context)
     return HttpResponse(HTML_STRING)
