33d87f77a45735ff1fbdfa177b633f7da0b6688f
CFE <hello@teamcfe.com>
2021-07-21 16:54:31 -0500

    21 - Register Model in the Admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title', 'content']
+
+admin.site.register(Article, ArticleAdmin)
