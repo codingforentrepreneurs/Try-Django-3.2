f83514d20d8561f8eb3560837c33cafeb7781713
CFE <hello@teamcfe.com>
2021-07-23 11:15:36 -0500

    27 - Basic Django Form

---
+++ articles/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+class ArticleForm(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField()
+
+    # def clean_title(self):
+    #     cleaned_data = self.cleaned_data # dictionary
+    #     print("cleaned_data", cleaned_data)
+    #     title = cleaned_data.get('title')
+    #     if title.lower().strip() == "the office":
+    #         raise forms.ValidationError('This title is taken.')
+    #     print("title", title)
+    #     return title
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        print('all data', cleaned_data)
+        title = cleaned_data.get('title')
+        content = cleaned_data.get("content")
+        if title.lower().strip() == "the office":
+            self.add_error('title', 'This title is taken.')
+            # raise forms.ValidationError('This title is taken.')
+        if "office" in content or "office" in title.lower():
+            self.add_error('content', "Office cannot be in content")
+            raise forms.ValidationError("Office is not allowed")
+        return cleaned_data
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .forms import ArticleForm
 from .models import Article
 # Create your views here.
 
@@ -24,14 +25,19 @@ def article_search_view(request):
 @login_required
 def article_create_view(request):
     # print(request.POST)
-    context = {}
+    form = ArticleForm()
+    context = {
+        "form": form
+    }
     if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        print(title, content)
-        article_object = Article.objects.create(title=title, content=content)
-        context['object'] = article_object
-        context['created'] = True
+        form = ArticleForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            title = form.cleaned_data.get("title")
+            content = form.cleaned_data.get("content")
+            article_object = Article.objects.create(title=title, content=content)
+            context['object'] = article_object
+            context['created'] = True
     return render(request, "articles/create.html", context=context)
 
 def article_detail_view(request, id=None):
