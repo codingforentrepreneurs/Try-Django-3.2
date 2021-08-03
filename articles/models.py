c7494fc20cb5e73d68122afd2a66a97c1c10b19e
CFE <hello@teamcfe.com>
2021-08-03 12:44:07 -0500

    38 - Django pre_save & post_save signals

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 from django.utils import timezone
 from django.utils.text import slugify
 # Create your models here.
@@ -15,8 +16,25 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         # obj = Article.objects.get(id=1)
         # set something
-        if self.slug is None:
-            self.slug = slugify(self.title)
+        # if self.slug is None:
+        #     self.slug = slugify(self.title)
         super().save(*args, **kwargs)
         # obj.save()
         # do another something
+
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    if instance.slug is None:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(article_pre_save, sender=Article)
+
+
+def article_post_save(sender, instance, created, *args, **kwargs):
+    print('post_save')
+    if created:
+        instance.slug = slugify(instance.title)
+        instance.save()
+
+post_save.connect(article_post_save, sender=Article)
