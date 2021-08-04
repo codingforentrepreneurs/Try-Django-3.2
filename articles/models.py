253dbce659b7bfa802b58ce3bd4240c8de5654a8
CFE <hello@teamcfe.com>
2021-08-04 16:21:52 -0500

    49 - Basic Data Connection with Foreign Keys

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
@@ -6,6 +7,8 @@ from django.utils import timezone
 # Create your models here.
 
 from .utils import slugify_instance_title
+
+User = settings.AUTH_USER_MODEL
 
 class ArticleQuerySet(models.QuerySet):
     def search(self, query=None):
@@ -24,6 +27,7 @@ class ArticleManager(models.Manager):
 class Article(models.Model):
     # https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
     # Django model-field-types
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
