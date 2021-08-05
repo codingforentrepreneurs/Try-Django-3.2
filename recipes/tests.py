d59185138c77604470bb7444ac9e11f16ca924b9
CFE <hello@teamcfe.com>
2021-08-05 16:35:05 -0500

    54 - Test Custom Model Validation

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -73,5 +74,27 @@ class RecipeTestCase(TestCase):
         ids = user.recipe_set.all().values_list("id", flat=True)
         qs = RecipeIngredient.objects.filter(recipe__id__in=ids)
         self.assertEqual(qs.count(), 1)
+
+    def test_unit_measure_validation(self):
+        invalid_unit = 'ounce'
+        ingredient = RecipeIngredient(
+            name='New',
+            quantity=10,
+            recipe=self.recipe_a,
+            unit=invalid_unit
+        )
+        ingredient.full_clean()
+
+    def test_unit_measure_validation_error(self):
+        invalid_units = ['nada', 'asdfadsf']
+        with self.assertRaises(ValidationError):
+            for unit in invalid_units:
+                ingredient = RecipeIngredient(
+                    name='New',
+                    quantity=10,
+                    recipe=self.recipe_a,
+                    unit=unit
+                )
+                ingredient.full_clean()
     
     
