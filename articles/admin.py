2684d48af78c8935cd3468deba6bcdbf711b08a4
CFE <hello@teamcfe.com>
2021-08-05 12:27:02 -0500

    51 - Admin Inlines for Foriegn Keys

--- articles/admin.py
+++ articles/admin.py
@@ -6,5 +6,6 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'slug', 'timestamp', 'updated']
     search_fields = ['title', 'content']
+    raw_id_fields = ['user']
 
 admin.site.register(Article, ArticleAdmin)
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.contrib import admin
 
 # Register your models here.
+from .models import RecipeIngredient, Recipe
+
+User = get_user_model()
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    # fields = ['name', 'quanity', 'unit', 'directions']
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+admin.site.register(Recipe, RecipeAdmin)
