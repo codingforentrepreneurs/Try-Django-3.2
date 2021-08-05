fb5344cf30ba08b59940ad932785f2482f689fee
CFE <hello@teamcfe.com>
2021-08-05 13:01:33 -0500

    52 - Understanding Relationships between Models via Tests

--- recipes/models.py
+++ recipes/models.py
@@ -25,13 +25,12 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
-    quanity = models.CharField(max_length=50)  # 1 1/4
+    quantity = models.CharField(max_length=50)  # 1 1/4
     unit = models.CharField(max_length=50) # pounds, lbs, oz, gram, etc
     directions = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True) 
     updated = models.DateTimeField(auto_now=True) 
     active = models.BooleanField(default=True)
-
 
 # class RecipeImage():
 #     recipe = models.ForeignKey(Recipe)
