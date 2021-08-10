bd443f6e99419b0d669f637854f2397aeb07cad0
CFE <hello@teamcfe.com>
2021-08-10 10:35:52 -0500

    62 - Customizing Form Fields, Widgets, Placeholders, & CSS Classes

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,9 +4,29 @@ from django import forms
 from .models import Recipe, RecipeIngredient
 
 class RecipeForm(forms.ModelForm):
+    error_css_class = 'error-field'
+    required_css_class = 'required-field'
+    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Recipe name"}))
+    # descriptions = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # django-crispy-forms
+        for field in self.fields:
+            new_data = {
+                "placeholder": f'Recipe {str(field)}',
+                "class": 'form-control'
+            }
+            self.fields[str(field)].widget.attrs.update(
+                new_data
+            )
+        # self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({'class': 'form-control-2'})
+        self.fields['description'].widget.attrs.update({'rows': '2'})
+        self.fields['directions'].widget.attrs.update({'rows': '4'})
 
 
 class RecipeIngredientForm(forms.ModelForm):
