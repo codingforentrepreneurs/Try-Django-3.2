18925333ffe7868665b859f53b3823d89fd5a05d
CFE <hello@teamcfe.com>
2021-08-10 14:29:22 -0500

    65 - Dynamic New Forms in Django Formset via HTMX

--- recipes/views.py
+++ recipes/views.py
@@ -60,4 +60,6 @@ def recipe_update_view(request, id=None):
             child.recipe = parent
             child.save()
         context['message'] = 'Data saved.'
+    if request.htmx:
+        return render(request, "recipes/partials/forms.html", context)
     return render(request, "recipes/create-update.html", context)  
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_htmx',
     'articles',
     'recipes'
 ]
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'trydjango.urls'
