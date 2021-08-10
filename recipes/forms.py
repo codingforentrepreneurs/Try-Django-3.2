8d2804506eb93c894fa85f796bb5d1a38bccc70c
CFE <hello@teamcfe.com>
2021-08-10 10:54:12 -0500

    63 - Rendering Form Fields & More in Templates

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,7 +6,7 @@ from .models import Recipe, RecipeIngredient
 class RecipeForm(forms.ModelForm):
     error_css_class = 'error-field'
     required_css_class = 'required-field'
-    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Recipe name"}))
+    name = forms.CharField(help_text='This is your help! <a href="/contact">Contact us</a>')
     # descriptions = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
     class Meta:
         model = Recipe
