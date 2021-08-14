928673eab9161cca60c4103311614c92a4643194
CFE <hello@teamcfe.com>
2021-08-14 11:20:40 -0500

    74 - Static Files in Development

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -156,6 +156,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production, we want cdn
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
