d0e1a51ee363112e0b49aaed8b73ecba4b3d5ee4
CFE <hello@teamcfe.com>
2021-09-28 16:28:27 -0500

    87 - Toggle Recipes into Meal Queue & Test

--- meals/models.py
+++ meals/models.py
@@ -20,9 +20,6 @@ class MealStatus(models.TextChoices):
     ABORTED = 'a', 'Aborted'
 
 
-MEAL_CHOICES = [
-    ('a', 'Aborted'),
-]
 
 class MealQuerySet(models.QuerySet):
     def by_user_id(self, user_id):
@@ -43,11 +40,37 @@ class MealQuerySet(models.QuerySet):
     def expired(self):
         return self.filter(status=MealStatus.EXPIRED)
 
+    def in_queue(self, recipe_id):
+        return self.pending().filter(recipe_id=recipe_id).exists()
+
    
 class MealManager(models.Manager):
     def get_queryset(self):
         return MealQuerySet(self.model, using=self._db)
 
+    def by_user_id(self, user_id):
+        return self.get_queryset().by_user_id(user_id)
+    
+    def by_user(self, user):
+        return self.get_queryset().by_user(user)
+
+    def toggle_in_queue(self, user_id, recipe_id):
+        qs = self.get_queryset().all().by_user_id(user_id)
+        already_queued = qs.in_queue(recipe_id)
+        added = None
+        if already_queued:
+            recipe_qs = qs.filter(recipe_id=recipe_id)
+            recipe_qs.update(status=MealStatus.ABORTED)
+            added = False
+        else:
+            obj = self.model(
+                user_id=user_id,
+                recipe_id=recipe_id,
+                status=MealStatus.PENDING
+            )
+            obj.save()
+            added = True
+        return added
 
 class Meal(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
