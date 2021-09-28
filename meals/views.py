b1cc8a3ca8ca029af8f85d46b42b6955ffb1b073
CFE <hello@teamcfe.com>
2021-09-28 17:36:35 -0500

    88 - Meal Queue Toggle View

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
-# Create your views here.
+from recipes.models import Recipe
+from .models import Meal
+
+def meal_queue_toggle_view(request, recipe_id=None):
+    if not request.htmx:
+        return HttpResponseBadRequest()
+    user = request.user
+    user_id = None
+    if not user.is_authenticated:
+        return HttpResponse("Must be logged in", status=400)
+    user_id = user.id
+    if user_id is None:
+        return 
+    if request.method == "POST":
+        is_valid_recipe = False
+        try:
+            recipe_obj = Recipe.objects.get(user=user, id=recipe_id)
+            is_valid_recipe = True
+        except:
+            pass
+        if is_valid_recipe:
+            Meal.objects.toggle_in_queue(user_id, recipe_id)
+    is_pending = Meal.objects.by_user_id(user_id).in_queue(recipe_id)
+    toggle_label = "Add to meals" if not is_pending else "Remove from meals"
+    context = {
+        'recipe_id': recipe_id, 
+        'toggle_label': toggle_label,
+        'is_pending': is_pending
+    }
+    return render(request, "meals/partials/queue-toggle.html", context)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -22,6 +22,8 @@ from accounts.views import (
     register_view
 )
 
+from meals.views import meal_queue_toggle_view
+
 from search.views import search_view
 from .views import home_view
 
@@ -29,6 +31,7 @@ urlpatterns = [
     path('', home_view), # index / home / root 
     path('pantry/recipes/', include('recipes.urls')),
     path('articles/', include('articles.urls')),
+    path('meal-toggle/<int:recipe_id>/', meal_queue_toggle_view, name='meal-toggle'),
     path('search/', search_view, name='search'),
     path('admin/', admin.site.urls),
     path('login/', login_view),
