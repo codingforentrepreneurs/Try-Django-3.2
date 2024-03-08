d6c9c7d0e703358cbe9c2672ba45084c114b6019
pravar1919 <pravar.sharma@gmail.com>
2024-03-08 14:00:52 +0530

    fixed typo

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,7 +8,7 @@ from .views import (
     recipe_update_view,
     recipe_detail_hx_view,
     recipe_ingredient_update_hx_view,
-    recipe_incredient_delete_view,
+    recipe_ingredient_delete_view,
     recipe_ingredient_image_upload_view
 
 
@@ -25,7 +25,7 @@ urlpatterns = [
     path("hx/<int:id>/", recipe_detail_hx_view, name='hx-detail'),
     
     path("<int:parent_id>/image-upload/", recipe_ingredient_image_upload_view, name='recipe-ingredient-image-upload'),
-    path("<int:parent_id>/ingredient/<int:id>/delete/", recipe_incredient_delete_view, name='ingredient-delete'),
+    path("<int:parent_id>/ingredient/<int:id>/delete/", recipe_ingredient_delete_view, name='ingredient-delete'),
     path("<int:id>/delete/", recipe_delete_view, name='delete'),
     path("<int:id>/edit/", recipe_update_view, name='update'),
     path("<int:id>/", recipe_detail_view, name='detail'),
